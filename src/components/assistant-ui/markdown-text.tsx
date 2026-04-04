a6cce29cf1f8144f9a8951798c314eadef5a5f46
Luis Canada <dev@luisc.me>
2026-04-04 13:47:07 -0400

    Fix syntax highlihgting, and add math

--- src/components/assistant-ui/markdown-text.tsx
+++ src/components/assistant-ui/markdown-text.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import { useAuiState, useMessagePartText } from "@assistant-ui/react";
-import { code } from "@streamdown/code";
+import { createCodePlugin } from "@streamdown/code";
+import { math } from '@streamdown/math';
+import { mermaid } from '@streamdown/mermaid';
 import { memo } from "react";
 import { Streamdown } from "streamdown";
+import 'katex/dist/katex.min.css';
 
 import { cn } from "#/lib/utils";
+
+const code = createCodePlugin({
+  themes: ['github-light', 'github-dark'], // [light, dark]
+});
 
 const MarkdownTextImpl = () => {
 	const part = useMessagePartText();
 	const isLastMessage = useAuiState((s) => s.message.isLast);
 	const isRunning = useAuiState((s) => s.message.status?.type === "running");
-	const caret = isRunning && isLastMessage ? "block" : undefined;
+	const caret = isRunning && isLastMessage ? "circle" : undefined;
 	const text =
 		part.type === "text" || part.type === "reasoning" ? part.text : "";
 
@@ -21,7 +28,7 @@ const MarkdownTextImpl = () => {
 			caret={caret}
 			className={cn("aui-md text-sm leading-6")}
 			isAnimating={isRunning}
-			plugins={{ code }}
+			plugins={{ code, math, mermaid }}
 			shikiTheme={["github-light", "github-dark"]}
 		>
 			{text}
